0359a479e70d0a943723d5d2b9aef217c24e96ca
jameslu255 <jameslu.255@gmail.com>
2020-01-28 14:58:42 -0800

    added page range class

---
+++ template/page_range.py
@@ -0,0 +1,15 @@
+
+
+class Range:
+#tail page array
+#base page array
+#1 rid for tail page
+#1 rid for base page both equal to 1
+    
+
+    def __init__(self):
+        self.tail_pages = []
+        self.base_pages = []
+        self.tail_rid = 1
+        self.base_rid = 1
+
