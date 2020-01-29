d612d61dd398107439ee64d47ab95fe4a2a7e980
mootechs <jodquach@ucdavis.edu>
2020-01-28 17:14:26 -0800

    New page class

--- template/page.py
+++ template/page.py
@@ -5,16 +5,14 @@ class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray()
 
     def has_capacity(self):
-        return (4096 - self.num_records) > 0
+        return (4096 - 8 * self.num_records) > 0
 
     def write(self, value):
         if self.has_capacity():
-            # data will be written at idx 4 since 
-            # the first 4 columns are reserved
-            self.data[self.num_records] = value
+            self.data += value.to_bytes(8, "big")
             self.num_records += 1
             return 0
         else:
