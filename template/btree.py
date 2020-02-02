a8273beea0b784c0f4879587c2ad6f46be020289
jameslu255 <jameslu.255@gmail.com>
2020-02-02 13:33:26 -0800

    adding bTree

---
+++ template/btree.py
@@ -0,0 +1,14 @@
+from BTrees.IIBTree import IIBTree
+from BTrees.OOBTree import OOBTree
+from BTrees.IOBTree import IOBTree
+from BTrees.OIBTree import OIBTree
+
+class BTree:
+    def __init__(self, num_assignments)
+        
+    def insertTree(tree, SID, RID):
+        tree.update({SID: RID})
+        #self.tree.update({1: 5})
+        #print(len(self.tree))
+
+BTree()
--- template/index.py
+++ template/index.py
@@ -1,15 +1,22 @@
-from template.table import Table
+#from template.table import Table
+from BTrees.IIBTree import IIBTree
 
 """
 # optional: Indexes the specified column of the specified table to speed up select queries
 # This data structure is usually a B-Tree
 """
 
+btree_list = []
+
 
 class Index:
 
     def __init__(self, table):
-        pass
+        for i in range(table.num_columns):
+            btree_list.append(IIBTree())
+            btree_list[0].update({1: 5})
+            print(list(btree_list[0].values()))
+        
 
     """
     # returns the location of all records with the given value
@@ -22,12 +29,12 @@ class Index:
     # optional: Create index on specific column
     """
 
-    def create_index(self, table, column_number):
+    def create_index(self, column_number):
         pass
 
     """
     # optional: Drop index of specific column
     """
 
-    def drop_index(self, table, column_number):
+    def drop_index(self, column_number):
         pass
