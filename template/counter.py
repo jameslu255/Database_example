01ef04b714a3314de233c32ce94e1343e6dc0db5
mootechs <jodquach@ucdavis.edu>
2020-03-05 13:52:03 -0800

    reverted changes

--- template/counter.py
+++ template/counter.py
@@ -4,16 +4,13 @@ class AtomicCounter:
     def __init__(self, initial=0):
     """Initialize a new atomic counter to given initial value (default 0)."""
         self.value = initial
-        self._rlock = threading.Lock()
-        self._wlock = threading.Lock()
-
-    def get(self):
+        self._lock = threading.Lock()
 
     def add(self, num=1):
         """Atomically increment the counter by num (default 1) and return the
         new value.
         """
-        with self._rlock:
+        with self._lock:
             self.value += num
             return self.value
 
