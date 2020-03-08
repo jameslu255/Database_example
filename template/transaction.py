6a03018fd573b38b0b1f4b1f5e21329cdb7d16c0
Brenda Lopez <yohanalopezrodas@gmail.com>
2020-03-07 16:34:50 -0800

    undo attempt

--- template/transaction.py
+++ template/transaction.py
@@ -7,7 +7,8 @@ class Transaction:
     """
     # Creates a transaction object.
     """
-    def __init__(self, id):
+    def __init__(self, id, query):
+        self.query = query
         self.queries = []
         
         self.id = id
@@ -73,19 +74,23 @@ class Transaction:
         # line_read = [ transaction id, what transactions occurred (could be 1+), [old val], [new val], base RID ]
         # line_read = ["1 update 0,0,0 10,20,30 512", "1 insert 0,0,0 1,2,3 513", ...]
         # we want the log to be in this format instead: 1 insert 0,0,0,0 2,3,4,5 1
-        read_array = line_read.split()  # ["1", "update", "[0,0,0]", "[0,0,0]", "RID"]
+        read_array = line_read.split()  # ["1", "update", "0,0,0", "0,0,0", "RID"]
         tid = int(read_array[0])
-        query = read_array[1]
+        query_str = read_array[1]
         old_values = self.parse_string_array(read_array[2])
         new_values = self.parse_string_array(read_array[2])
         base_RID = int(read_array[4])
 
-        if query == "update":
-            #TODO
-        elif query == "insert":
-            #TODO
-        elif query == "delete":
-            #TODO
+        table = self.query.table
+        key = table.keys{base_RID}
+
+        if query_str == "update":
+            # To undo an update: update w/ old values
+            self.query.update(key, *old_values)
+        elif query_str == "insert":
+            self.query.delete(*key)
+        elif query_str == "delete":
+            self.query.insert(*old_values)
 
 
 
